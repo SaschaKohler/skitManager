a2484469b61b385058fb87b18107db74c44aebde
SaschaKohler <sascha.kohler@sent.at>
2022-10-30 20:27:25 +0100

    refactoring

---
+++ lang/vendor/filament/de/resources/event-resource.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return [
+
+    "event_data" => "Baustellen-Details",
+    "title" => "Baustelle",
+    "calendar_type" => "Kategorie",
+    "start" => "Beginn",
+    "end" => "Ende",
+    "all_day" => "ganztägig",
+    "recurrence" => "Wiederholung",
+    "attachments" => "Anhang",
+    "images" => "Bilder",
+    "client_detail" => "Kunden-Details",
+    "client" => "Kunde"
+];

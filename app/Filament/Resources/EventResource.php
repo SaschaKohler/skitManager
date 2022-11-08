730926bad022c1a4974e022730b91139f5c8e079
SaschaKohler <sascha.kohler@sent.at>
2022-11-08 06:35:27 +0100

    get austrian zip codes

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -203,6 +203,7 @@ class EventResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
                 Tables\Actions\ForceDeleteBulkAction::make(),
             ]);
     }
